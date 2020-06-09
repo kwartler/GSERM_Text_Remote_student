22e3c64d707dee6c57fe5e5ea013ff2b8a13e78c
Edward Kwartler <edward.kwartler@edward.kwartler.MacBook>
2020-06-09 19:16:45 -0400

    add instructions to hw3

---
+++ lessons/C_Sentiment_Unsupervised/HW/D3_HW_Instructions.R
@@ -0,0 +1,26 @@
+#' Title: Day 3 Homework
+#' Purpose: Testing your knowledge of sentiment/polarity and unsuprevised approaches
+#' Author: Ted Kwartler
+#' email: edwardkwartler@fas.harvard.edu
+#' License: GPL>=3
+#' Date: June-9-2020
+#' 
+
+## Your script header should be the following:
+#' Title: GSERM REMOTE DAY 3 HW
+#' Purpose: 20pts, evaluate polarity/sentiment/clustering
+#' NAME: 
+#' Date: 
+
+## Keep in mind, there isn't a right, wrong answer here.  This text is small, and we are evaluating the code execution and appropriate sections, steps being taken more than results on this toy data.
+
+# HW Tasks:
+# 1. Apply 1 sentiment lexicon or polarity analysis to news.csv data.
+# 2. Perform 1 clustering method with the news.csv data. 
+# Considerations:  
+# - The title, description and content columns are all ok to use as well as the combined title_description_content column.  
+# - There is a column called newsSite with sources cnn, fox-news, msnbc, the-american-conservative.  You may analyze each news sources individually for your analysis (full credit) or into two groups i.e. right or left leaning (-1pt), or treat them as a single group therby ignoring the column and just treating it as a single document set (-2 pts because its easiest)
+
+
+
+# End
